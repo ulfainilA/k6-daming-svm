ed8e62bb3ed43a9db5e145616f316dc6098ef1bc
Ulfa <ulfainil_a41@apps.ipb.ac.id>
2021-06-13 17:44:57 +0700

    Update svm model knn.r

--- svm model knn.r
+++ svm model knn.r
@@ -1,14 +1,6 @@
 data <- read.csv("train.csv", header = TRUE, sep = ",")
 str(data)
 summary(data)
-sd(data$battery_power)
-sd(data$blue)
-sd(data$dual_sim)
-sd(data$fc)
-sd(data$four_g)
-sd(data$int_memory)
-sd(data$m_dep)
-sd(data$mobile_wt)
 
 boxplot(data$battery_power, main="Boxplot of Battery Power",
         ylab="battery_power")
@@ -27,26 +19,7 @@ library(mice)
 md.pattern(data)
 
 
-#melakukan diskretisasi pada atribut price dengan membagi menjadi 3 kategori
-#menggunakan metode equal width
-library(infotheo)
-ew.price <- discretize(data$price_range, "equalwidth", 3)
-ew.price
-
-#Gabungkan hasil diskretisasi dengan data hasil praproses
-ew.price$x = as.factor(ew.price$X) #menggabungkan atribut baru kedalam data
-data$price_discre = ew.price$X #memberi nama pada atribut baru
-data
-
-#transformasi
-data$price_trans<-NULL
-data$price_trans[data$price_discre == 1 ] <- "low"
-data$price_trans[data$price_discre == 2 ] <- "medium"
-data$price_trans[data$price_discre == 3 ] <- "high"
-data
-
 #cek outlier
-
 outliers <- boxplot(data$battery_power, plot=FALSE)$out
 outliers2 <- boxplot(data$clock_speed, plot=FALSE)$out
 outliers3 <- boxplot(data$fc, plot=FALSE)$out
@@ -66,23 +39,6 @@ outliers12 <- boxplot(data$sc_h, plot=FALSE)$out
 outliers13 <- boxplot(data$sc_w, plot=FALSE)$out
 outliers14 <- boxplot(data$talk_time, plot=FALSE)$out
 
-#normalisasi data
-norm <-function(x){
-  (x-min(x))/(max(x)-min(x))
-}
-data$fc = norm(data$fc)
-data$clock_speed = norm(data$clock_speed)
-data$int_memory = norm(data$int_memory)
-data$mobile_wt = norm(data$mobile_wt)
-data$n_cores = norm(data$n_cores)
-data$pc = norm(data$pc)
-data$px_height = norm(data$px_height)
-data$px_width = norm(data$px_width)
-data$ram = norm(data$ram)
-data$sc_h = norm(data$sc_h)
-data$sc_w = norm(data$sc_w)
-data$talk_time = norm(data$talk_time)
-
 library(class)
 library(caret)
 library(e1071)
@@ -92,10 +48,9 @@ library(ggplot2)
 library(scales)
 library(corrplot)
 
-unique(data$price_discre)
+unique(data$price_range)
 
-data <- subset(data, select = -price_trans)
-data <- subset(data, select = -price_range)
+#data <- subset(data, select = -price_range)
 
 a <-cor(data)
 corrplot(a, method="color")
@@ -112,19 +67,18 @@ data$four_g <- as.numeric(data$four_g)
 data$touch_screen <-as.numeric(data$touch_screen)
 #data$price_range <- as.numeric(data$price_range)
 
-smp_size <- floor(0.75 * nrow(data))
+smp_size <- floor(0.7 * nrow(data))
 
-# set the seed to make our partition reproducible
 set.seed(123)
 train_ind <- sample(seq_len(nrow(data)), size = smp_size)
 
 train <- data[train_ind, ]
 valid <- data[-train_ind, ]
 
-x_train <- subset(train, select = -price_discre)
-y_train <- train$price_discre
-x_valid <- subset(valid, select = -price_discre)
-y_valid <- valid$price_discre
+x_train <- subset(train, select = -price_range)
+y_train <- train$price_range
+x_valid <- subset(valid, select = -price_range)
+y_valid <- valid$price_range
 
 
 #SVM Model
@@ -134,7 +88,6 @@ model <- svm(x_train,y_train, type = 'C-classification',
 print(model)
 summary(model)
 
-
 #evaluasi model
 predvalid <- predict(model, x_valid)
 predvalid <- as.factor(predvalid)
@@ -142,7 +95,7 @@ predvalid <- as.factor(predvalid)
 y_valid <- as.factor(y_valid)
 confusionMatrix(y_valid,predvalid)
 
-outOfSampleAccuracy <- sum(predvalid == valid$price_discre)/length(predvalid)
+outOfSampleAccuracy <- sum(predvalid == valid$price_range)/length(predvalid)
 outOfSampleError <- (1 - outOfSampleAccuracy)
 print(outOfSampleAccuracy)
 print(outOfSampleError)
@@ -152,21 +105,12 @@ print(outOfSampleError)
 sampletest <- floor(0.5 * nrow(data))
 test_ind <- sample(seq_len(nrow(data)), size=sampletest)
 test <- data[test_ind, ]
-test <- subset(test, select = -price_discre)
+test <- subset(test, select = -price_range)
 test
 
-
 predtest <- predict(model, test)
-print(predtest)
+predtest
 
 price_test <- c(predtest)
 test$price_test <- price_test
-
-#transformasi
-test$price_trans<-NULL
-test$price_trans[test$price_test == 1 ] <- "low"
-test$price_trans[test$price_test == 2 ] <- "medium"
-test$price_trans[test$price_test == 3 ] <- "high"
-
-test <- subset(test, select = -price_test)
 test
